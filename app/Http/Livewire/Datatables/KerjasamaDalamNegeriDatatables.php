9463f4fa9f8ed6790930b1749633eafc25939ae1
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-09-17 14:49:08 +0800

    Penambahan fitur filtering pada iku-6

--- app/Http/Livewire/Datatables/KerjasamaDalamNegeriDatatables.php
+++ app/Http/Livewire/Datatables/KerjasamaDalamNegeriDatatables.php
@@ -8,15 +8,52 @@ use App\Models\Prodi;
 class KerjasamaDalamNegeriDatatables extends Component
 {
 
-    public $referenceCounts;
+    public $referenceCounts, $kerjasamaId, $orderBy, $orderDirection;
+    public $orderByText, $orderDirectionText, $kerjaSamaText ;
 
     public function mount()
     {
-        $this->referenceCounts = Prodi::getReferenceCounts();
+        $orderBy = "prodi_id";
+        $orderDirection = "asc";
+        $this->orderByText = 'Urut Berdasarkan';
+        $this->kerjaSamaText = 'All';
+        $this->referenceCounts = Prodi::getReferenceCounts($this->kerjasamaId, $orderBy, $orderDirection);
     }
 
+    public function updated()
+    {
+        $this->referenceCounts = Prodi::getReferenceCounts($this->kerjasamaId, $this->orderBy, $this->orderDirection);
+    }
     public function render()
     {
-        return view('livewire.datatables.kerjasama-dalam-negeri-datatables');
+        return view('livewire.datatables.kerjasama-dalam-negeri-datatables', [
+            'referenceCounts' => $this->referenceCounts,
+            'orderBy' => $this->orderBy,
+            'orderByText' => $this->orderByText,
+            'kerjasamaId' => $this->kerjasamaId,
+            'kerjaSamaText' => $this->kerjaSamaText,
+            'orderDirection' => $this->orderDirection,
+            'orderDirectionText' => $this->orderDirectionText
+            
+         ]);
+    }
+
+    public function setKerjasamaId($id, $text)
+    {
+        $this->kerjasamaId = $id;
+        $this->kerjaSamaText = $text;
+        $this->updated();
+    }
+
+    public function setOrderBy($column, $text)
+    {
+        $this->orderBy = $column;
+        $this->orderByText = $text;
+        $this->updated();
+    }
+
+    public function setOrderDirection(){
+        $this->orderDirection = $this->orderDirection === "asc" ? "desc" : "asc"  ;
+        $this->updated();
     }
 }
--- resources/views/livewire/datatables/kerjasama-dalam-negeri-datatables.blade.php
+++ resources/views/livewire/datatables/kerjasama-dalam-negeri-datatables.blade.php
@@ -2,29 +2,34 @@
     <h5 class="card-header bg-primary p-3 text-white">Data Table</h5>
     <div class="card-body">
         <div class="d-flex flex-row-reverse bd-highlight mt-3">
+          <div class="dropdown order_direction px-3">
+            <button class="btn btn-outline-primary" wire:click.debounce.300ms="setOrderDirection">
+              <i class="menu-icon tf-icons bx {{ $orderDirection === 'asc' ? 'bx-sort-up' : 'bx-sort-down' }}"></i>
+          </button>          
+        </div> 
             <div class="dropdown jenis_kerja_sama px-3">
-                <button class="btn btn-outline-primary dropdown-toggle "  role="button" data-bs-toggle="dropdown"
+                <button class="btn btn-outline-primary dropdown-toggle width" style="width: 200px;" role="button" data-bs-toggle="dropdown"
                 >
-                  Jenis Kerja Sama
+                    {{ $kerjaSamaText }}
                 </button>
               
-                <ul class="dropdown-menu ">
-                  <li><a class="dropdown-item" >All</a></li>
-                  <li><a class="dropdown-item" >Dalam Negeri</a></li>
-                  <li><a class="dropdown-item" >Luar Negeri</a></li>
-                </ul>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" wire:click="setKerjasamaId(null, 'All')">All</a></li>
+                  <li><a class="dropdown-item" wire:click="setKerjasamaId(1, 'Dalam Negeri')">Dalam Negeri</a></li>
+                  <li><a class="dropdown-item" wire:click="setKerjasamaId(2, 'Luar Negeri')">Luar Negeri</a></li>
+              </ul>
             </div> 
             <div class="dropdown order_by ">
-                <button class="btn btn-outline-primary dropdown-toggle "  role="button" data-bs-toggle="dropdown" >
-                  Urut Berdasarkan
+                <button class="btn btn-outline-primary dropdown-toggle" style="width: 200px;" role="button" data-bs-toggle="dropdown" >
+                  {{ $orderByText }}
                 </button>
               
                 <ul class="dropdown-menu text-primary">
-                    <li class="dropdown-item">PRODI</li>
-                    <li class="dropdown-item">MoA</li>
-                    <li class="dropdown-item">IA</li>
-                    <li class="dropdown-item">MoU</li>
-                    <li class="dropdown-item">TOTAL KERJA SAMA</li>
+                    <li class="dropdown-item" wire:click="setOrderBy('prodi_id', 'Prodi')">PRODI</li>
+                    <li class="dropdown-item" wire:click="setOrderBy('moa_reference_count', 'MoA')">MoA</li>
+                    <li class="dropdown-item" wire:click="setOrderBy('ia_reference_count', 'IA')">IA</li>
+                    <li class="dropdown-item" wire:click="setOrderBy('mou_reference_count', 'MoU')">MoU</li>
+                    <li class="dropdown-item" wire:click="setOrderBy('total_reference_count', 'Total Kerja Sama')">TOTAL KERJA SAMA</li>
                 </ul>
               </div>  
         </div>
@@ -45,10 +50,10 @@
                     <tr style="font-size: 11px">
                         <td>{{$referenceCount->prodi_id }}</td>
                         <td>{{$referenceCount->prodi_name}}</td>
-                        <td>{{ $referenceCount->moa_reference_count }}</td>
-                        <td>{{ $referenceCount->ia_reference_count }}</td>
-                        <td>{{ $referenceCount->mou_reference_count }}</td>
-                        <td>{{ $referenceCount->total_reference_count }}</td>
+                        <td>{{ $referenceCount->moa_reference_count ?? 0 }}</td>
+                        <td>{{ $referenceCount->ia_reference_count ?? 0}}</td>
+                        <td>{{ $referenceCount->mou_reference_count ?? 0}}</td>
+                        <td>{{ $referenceCount->total_reference_count ?? 0 }}</td>
                     </tr>
                     @endforeach
                 </tbody>
