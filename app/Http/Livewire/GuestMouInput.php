004923d83d11c32dd5a384d6d3b26b1d3fffa8ed
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-10-18 14:01:55 +0800

    Penambahan component input mou guest user

---
+++ app/Http/Livewire/GuestMouInput.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class GuestMouInput extends Component
+{
+    public function render()
+    {
+        return view('livewire.guest-mou-input');
+    }
+}
