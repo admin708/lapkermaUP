5e9ac28d87ef57751bc3e7197b30ccbd61e50f6c
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-10-18 13:06:09 +0800

    Filter kejasama tanpa laporan

--- app/Http/Livewire/Datatables/IkuScores.php
+++ app/Http/Livewire/Datatables/IkuScores.php
@@ -49,11 +49,13 @@ class IkuScores extends Component
                     FROM data_moa dm
                     LEFT JOIN data_moa_penggiat dmp ON dm.id = dmp.id_lapkerma
                     LEFT JOIN referensi_badan_kemitraans rbk ON rbk.id = dmp.badan_kemitraan
+                    WHERE dm.laporan IS NOT NULL AND dm.laporan != ""
                     GROUP BY dm.prodi_id) as moa_skor'), 'moa_skor.prodi_id', '=', 'prodis.id')
             ->leftJoin(DB::raw('(SELECT di.prodi_id, SUM(rbki.bobot) AS skor
                     FROM data_ia di
                     LEFT JOIN data_ia_penggiat dip ON di.id = dip.id_lapkerma
                     LEFT JOIN referensi_badan_kemitraans rbki ON rbki.id = dip.badan_kemitraan
+                    WHERE di.laporan IS NOT NULL AND di.laporan != ""
                     GROUP BY di.prodi_id) as ia_skor'), 'ia_skor.prodi_id', '=', 'prodis.id')
             ->where('prodis.jenjang', 'sarjana')
             ->groupBy('prodis.id', 'prodis.nama_resmi')
--- resources/views/livewire/datatables/iku-scores.blade.php
+++ resources/views/livewire/datatables/iku-scores.blade.php
@@ -2,7 +2,7 @@
     <h5 class="card-header bg-primary p-3 text-white">Data Table</h5>
     <div class="card-body">
         <div class="table-responsive text-nowrap mt-3">
-            <div class="dropdown jenis_kerja_sama">
+            {{-- <div class="dropdown jenis_kerja_sama">
                 <button class="btn btn-outline-primary dropdown-toggle width" style="width: 200px;" role="button"
                     data-bs-toggle="dropdown">
                     Pilih Jenis Kerja Sama
@@ -12,7 +12,7 @@
                     <li><a class="dropdown-item" wire:click="setKerjasamaId(1, 'Dalam Negeri')">FAKULTAS</a></li>
                     <li><a class="dropdown-item" wire:click="setKerjasamaId(2, 'Luar Negeri')">UNIVERSITAS</a></li>
                 </ul>
-            </div>
+            </div> --}}
             <table class="table table-bordered table-hover table-sm text-start mt-2" style="font-size: 13px">
                 <thead>
                     <tr>
