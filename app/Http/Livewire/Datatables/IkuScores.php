d69ece8d501557309a54415c3bf32d453ea9a1f1
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-10-18 11:29:27 +0800

    fix table direction

--- app/Http/Livewire/Datatables/IkuScores.php
+++ app/Http/Livewire/Datatables/IkuScores.php
@@ -38,23 +38,23 @@ class IkuScores extends Component
 
     public function getData()
     {
-        $results = Prodi::select('prodis.id as prodi_id', 'prodis.nama_resmi as nama_resmi', DB::raw('(
-            (COALESCE(moa_skor.skor, 0) + COALESCE(ia_skor.skor, 0)) / (
-                SELECT COUNT(*) 
-                FROM prodis 
-                WHERE jenjang = "sarjana"
-            ) * 100
-        ) AS skor_iku'))
+        $results = Prodi::select('prodis.id as prodi_id', 'prodis.nama_resmi', DB::raw('ROUND((
+        (COALESCE(moa_skor.skor, 0) + COALESCE(ia_skor.skor, 0)) / (
+            SELECT COUNT(*) 
+            FROM prodis 
+            WHERE jenjang = "sarjana"
+        ) * 100
+    ), 2) AS skor_iku')) // Round to 2 decimal places
             ->leftJoin(DB::raw('(SELECT dm.prodi_id, SUM(rbk.bobot) AS skor
-                        FROM data_moa dm
-                        LEFT JOIN data_moa_penggiat dmp ON dm.id = dmp.id_lapkerma
-                        LEFT JOIN referensi_badan_kemitraans rbk ON rbk.id = dmp.badan_kemitraan
-                        GROUP BY dm.prodi_id) as moa_skor'), 'moa_skor.prodi_id', '=', 'prodis.id')
+                    FROM data_moa dm
+                    LEFT JOIN data_moa_penggiat dmp ON dm.id = dmp.id_lapkerma
+                    LEFT JOIN referensi_badan_kemitraans rbk ON rbk.id = dmp.badan_kemitraan
+                    GROUP BY dm.prodi_id) as moa_skor'), 'moa_skor.prodi_id', '=', 'prodis.id')
             ->leftJoin(DB::raw('(SELECT di.prodi_id, SUM(rbki.bobot) AS skor
-                        FROM data_ia di
-                        LEFT JOIN data_ia_penggiat dip ON di.id = dip.id_lapkerma
-                        LEFT JOIN referensi_badan_kemitraans rbki ON rbki.id = dip.badan_kemitraan
-                        GROUP BY di.prodi_id) as ia_skor'), 'ia_skor.prodi_id', '=', 'prodis.id')
+                    FROM data_ia di
+                    LEFT JOIN data_ia_penggiat dip ON di.id = dip.id_lapkerma
+                    LEFT JOIN referensi_badan_kemitraans rbki ON rbki.id = dip.badan_kemitraan
+                    GROUP BY di.prodi_id) as ia_skor'), 'ia_skor.prodi_id', '=', 'prodis.id')
             ->where('prodis.jenjang', 'sarjana')
             ->groupBy('prodis.id', 'prodis.nama_resmi')
             ->orderBy('prodis.id', 'asc')
@@ -62,6 +62,7 @@ class IkuScores extends Component
 
         return $results;
     }
+
 
 
 
